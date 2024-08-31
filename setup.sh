a9c96239e2a0a0dbcaece3ffaf390e053087bacd
Tirth2742 <tirthofficial2742.com>
2024-08-31 15:09:19 +0530

    first commit

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ apt-get install -y python3-distutils
 # Install pip if not installed and upgrade it
 apt-get install -y python3-pip
 pip install --upgrade pip
+
+# Install additional dependencies
+apt-get install -y build-essential
